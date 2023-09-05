872199d9a78731d059d7a7858d3ee8af32d949e3
jmorgainz <joshua.morgan.elite@gmail.com>
2023-09-05 16:16:05 -0400

    Database is done

--- models/mealPlan.js
+++ models/mealPlan.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
 // Define the 'meals' schema
-const mealSchema = new mongoose.Schema({
-  name: String,
-  calories: Number,
-  price: Number,
-});
+// const mealSchema = new mongoose.Schema({
+//   name: String,
+//   calories: Number,
+//   price: Number,
+// });
 
 // Define the 'mealPlan' schema with an embedded 'meals' array
 const mealPlanSchema = new mongoose.Schema({
@@ -18,27 +18,8 @@ const mealPlanSchema = new mongoose.Schema({
 });
 
 // Create models for 'Meal' and 'MealPlan'
-const Meal = mongoose.model('Meal', mealSchema);
+// const Meal = mongoose.model('Meal', mealSchema);
 const MealPlan = mongoose.model('MealPlan', mealPlanSchema);
-
-// Create meal plans
-const plan1 = new MealPlan({
-  name: 'Plan 1',
-  price: 30.99,
-//   meals: [meal1._id, meal2._id],
-});
-
-const plan2 = new MealPlan({
-  name: 'Plan 2',
-  price: 25.99,
-//   meals: [meal2._id],
-});
-
-// // Save meals and meal plans to the database
-// meal1.save();
-// meal2.save();
-// plan1.save();
-// plan2.save();
 
 // You can now query meal plans and populate the 'meals' field to retrieve the embedded meal data when needed
 MealPlan.find({}).populate('selectedMeals')
--- models/seed.js
+++ models/seed.js
@@ -1,34 +1,35 @@
 const mongoose = require('mongoose');
 const Meal = require('./meal');
+const MealPlan = require('./mealPlan');
 // const Meal = require('./mealPlan');
-const mealsData = require('./data.json');
+const mealsData = require('./meals.json');
+const mealPlanData = require('./mealPlan.json');
  
 const db = require('../config/database');
 
 // mongoose.connect('mongodb+srv://jmorgainz:RockyTop5440@cluster0.wa9b2f4.mongodb.net/lean-gainz-meal-prep?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
-
 // Insert the meals into the database
 async function seedMeals() {
   try {
     await Meal.deleteMany(); 
     await Meal.insertMany(mealsData); 
     console.log('Meals seeded successfully');
+    await MealPlan.deleteMany(); 
+    await MealPlan.insertMany(mealPlanData); 
+    console.log('Meal Plan seeded successfully');
   } catch (error) {
     console.error('Error seeding meals:', error);
   } finally {
     mongoose.connection.close();
   }
 }
-
-// Run the seedMeals function to populate the database
-seedMeals();
+// seedMeals();
 
 // Mealplans Seed!!!
-// Insert the meals into the database
 async function seedMealPlans() {
     try {
-      await Meal.deleteMany(); 
-      await Meal.insertMany(mealPlanData); 
+      await MealPlan.deleteMany(); 
+      await MealPlan.insertMany(mealPlanData); 
       console.log('Meal Plan seeded successfully');
     } catch (error) {
       console.error('Error seeding meal Plan:', error);
@@ -36,8 +37,9 @@ async function seedMealPlans() {
       mongoose.connection.close();
     }
   }
-  
-  // Run the seedMeals function to populate the database
-  seedMealPlans();
+
+  seedMeals();
+
+    // seedMealPlans();
 
 // module.exports = mongoose.model('mealsData')
